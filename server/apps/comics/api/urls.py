9d3f08c149a724978c86530a29058aee2103bcf8
NowSpring <kodai218@icloud.com>
2024-01-25 01:42:42 +0900

    ホーム画面にコミックインスタンスをv-cardで表示

--- server/apps/comics/api/urls.py
+++ server/apps/comics/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from comics.api import views
 
+router = DefaultRouter()
+router.register(r'comics', views.ComicViewSet)
+
 urlpatterns = [
-    path("comics/", views.ComicListView.as_view(), name = "comic_list"),
+    path("", include(router.urls)),
+    # path("comics/", views.ComicListView.as_view(), name = "comic_list"),
     path("comics/<int:pk>/", views.ComicDetailView.as_view(), name = "comic_detail"),    
 ]
--- server/apps/comics/api/views.py
+++ server/apps/comics/api/views.py
@@ -1,6 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from comics.models import Comic
 from comics.api.serializers import ComicSerializer
+
+
+
+class ComicViewSet(viewsets.ModelViewSet):
+    queryset = Comic.objects.all()
+    serializer_class = ComicSerializer
 
 
 class ComicListView(generics.ListCreateAPIView):
