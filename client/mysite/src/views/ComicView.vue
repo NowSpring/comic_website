bbfd805f4e23eeb60508e0e8b94582557b136bdd
NowSpring <kodai218@icloud.com>
2024-01-30 03:38:42 +0900

    アイコン表示修正

--- client/mysite/src/views/ComicView.vue
+++ client/mysite/src/views/ComicView.vue
@@ -4,12 +4,12 @@
       <div class="d-flex justify-end">
         <div><!-- ダミーの div を入れると右にも寄せられる -->
           <v-btn
-            color="orange-lighten-2"
-            variant="text"
-            class="column-button"
+            icon
             to="/editor"
           >
-            +
+            <v-icon large>
+              mdi-book-plus-outline
+            </v-icon>
           </v-btn>
         </div>
       </div>
@@ -45,22 +45,45 @@
 
               <v-spacer></v-spacer>
 
-              <v-btn 
-                class="btn"
-                @click="card.show = !card.show"
-              >
-                <v-icon v-if="card.show">mdi-chevron-up</v-icon>
-                <v-icon v-else>mdi-chevron-down</v-icon>
+              <v-btn icon>
+                <v-icon
+                  v-if="card.show" 
+                  @click="card.show = !card.show"
+                >mdi-chevron-up</v-icon>
+                <v-icon
+                  v-else
+                  @click="card.show = !card.show"
+                >mdi-chevron-down</v-icon>
               </v-btn>
-
             </v-card-actions>
 
             <v-expand-transition>
               <div v-show="card.show">
                 <v-divider></v-divider>
 
+                <!-- <v-tabs v-model="infos">
+                  <v-tab value="info">Info</v-tab>
+                  <v-tab value="review">Review</v-tab>
+                </v-tabs> -->
+
                 <v-card-text>
-                  I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                  <v-row>
+                    <v-col cols="6">テスト</v-col>
+                    <v-col cols="6">テスト</v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col cols="6">テスト</v-col>
+                    <v-col cols="6">テスト</v-col>
+                  </v-row>
+                  <!-- <v-window v-model="infos">
+                    <v-window-item value="info">
+                        info test
+                    </v-window-item>
+
+                    <v-window-item value="review">
+                      review test
+                    </v-window-item>
+                  </v-window> -->
                 </v-card-text>
               </div>
             </v-expand-transition>
